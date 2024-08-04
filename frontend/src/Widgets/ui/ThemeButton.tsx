294bc225966d20898e42aec9570b8407a9addb4a
qalqa <andreybas04@gmail.com>
2024-08-04 20:00:02 +0300

    fix: fixed theme button

--- frontend/src/Widgets/ui/ThemeButton.tsx
+++ frontend/src/Widgets/ui/ThemeButton.tsx
@@ -5,7 +5,10 @@ const ThemeButton: React.FC = () => {
 	const { theme, toggleTheme } = useTheme()
 
 	return (
-		<button className='fixed bottom-4 right-4 h-12 w-12' onClick={toggleTheme}>
+		<button
+			className='fixed bottom-4 right-4 h-12 w-12 z-10'
+			onClick={toggleTheme}
+		>
 			<img
 				src={theme === 'light' ? '../../../moon.svg' : '../../../sun.svg'}
 				alt={'toggle to ' + theme + 'theme'}
