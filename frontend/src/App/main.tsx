8bfb1481ce742c75969d972a38e61df0cb860e2f
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2024-08-08 01:05:09 +0300

    initial commit

--- frontend/src/App/main.tsx
+++ frontend/src/App/main.tsx
@@ -4,12 +4,13 @@ import { ThemeProvider } from '../Features/ui/ThemeContext/ThemeContext.tsx'
 import Landing from '../Pages/Landing.tsx'
 import ThemeButton from '../Widgets/ui/ThemeButton.tsx'
 import './index.css'
+import Table from '../components/Table'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<ThemeProvider>
 			<ThemeButton />
-			<Landing />
+			<Table />
 		</ThemeProvider>
 	</React.StrictMode>
 )
