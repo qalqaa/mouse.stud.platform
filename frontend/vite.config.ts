93e5001ac736814bcaf8f8bb40e231aeb8682b3e
f.gugnin <f.gugnin@vk.team>
2024-08-07 15:33:11 +0300

    chore: cummed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+    hmr: { host: '0.0.0.0' },
+  },
 })
