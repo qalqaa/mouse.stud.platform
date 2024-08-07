df367fd47b62ace3a8bc5e7671547a6f8d9e56d1
f.gugnin <f.gugnin@vk.team>
2024-08-07 15:39:43 +0300

    chore: uncummed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  server: {
-    host: '0.0.0.0',
-    watch: {
-      usePolling: true,
-    },
-    hmr: { host: '0.0.0.0' },
-  },
+  plugins: [react()]
 })
