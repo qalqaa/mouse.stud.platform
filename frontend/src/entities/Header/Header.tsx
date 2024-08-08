178cc2bf150cb74ffad58c35348a0cfeee0202fc
qalqa <andreybas04@gmail.com>
2024-08-08 05:24:27 +0300

    fix: small fix header

--- frontend/src/entities/Header/Header.tsx
+++ frontend/src/entities/Header/Header.tsx
@@ -10,16 +10,18 @@ const Header: React.FC<IHeaderProps> = ({ theme }) => {
 	return (
 		<div className='header-container'>
 			<ul className='header-list h-screen px-3 py-10 gap-10 flex flex-col items-center'>
-				<img
-					className='w-16'
-					src={
-						theme === 'light'
-							? `../../../../react-logo.svg`
-							: `../../../../react-logo-inversed.svg`
-					}
-					alt=''
-				/>
-				<p className='version'>v{REACT_APP_VERSION}</p>
+				<div className='flex flex-col gap-3'>
+					<img
+						className='w-16'
+						src={
+							theme === 'light'
+								? `../../../../react-logo.svg`
+								: `../../../../react-logo-inversed.svg`
+						}
+						alt=''
+					/>
+					<p className='version'>v{REACT_APP_VERSION}</p>
+				</div>
 				<li className='flex flex-col justify-center gap-3'>
 					<img className='h-5' src='../../../profile.svg' alt='' />
 					<a>Профиль</a>
