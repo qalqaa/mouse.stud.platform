90698ca2623590b2fe0e3389d1aee125b8ef39e8
qalqa <andreybas04@gmail.com>
2024-08-08 05:23:10 +0300

    feat: if not authorized dummy

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -6,9 +6,17 @@ import './Home.css'
 const Home: React.FC = () => {
 	const { theme } = useTheme()
 	return (
-		<div>
+		<div className='homepage-container flex'>
 			<Header theme={theme} />
-			<h2>Home Page</h2>
+			<div className='w-full h-screen'>
+				<div className='ifNotAuthorized flex flex-col gap-5 h-screen justify-center items-center'>
+					<h3>
+						Внимание, чтобы продолжить,
+						<br /> необходимо авторизоваться через GitHub
+					</h3>
+					<button className='w-1/4'>Авторизоваться</button>
+				</div>
+			</div>
 		</div>
 	)
 }
