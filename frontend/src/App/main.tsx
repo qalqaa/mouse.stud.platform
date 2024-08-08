c217c13c4671d5c3f99617767bd2c189845fc88c
qalqa <andreybas04@gmail.com>
2024-08-08 03:33:58 +0300

    feat: Redux-toolkit setup

--- frontend/src/App/main.tsx
+++ frontend/src/App/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from '../app/store/store.ts'
 import { ThemeProvider } from '../features/ui/ThemeContext/ThemeContext.tsx'
+import './index.css'
+
 import Landing from '../pages/Landing.tsx'
 import ThemeButton from '../widgets/ui/ThemeButton.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<ThemeProvider>
-			<ThemeButton />
-			<Landing />
+			<Provider store={store}>
+				<ThemeButton />
+				<Landing />
+			</Provider>
 		</ThemeProvider>
 	</React.StrictMode>
 )
