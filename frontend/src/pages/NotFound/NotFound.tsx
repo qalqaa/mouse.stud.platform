ebd46498af90acc425738a8bafb19d224d28c0e7
qalqa <andreybas04@gmail.com>
2024-08-11 04:47:00 +0300

    feat: added QOL features

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound: React.FC = () => {
+	const navigate = useNavigate()
 	return (
-		<div className='h-screen flex flex-col justify-center'>
+		<div className='h-screen flex flex-col items-center gap-3 justify-center'>
 			<h2>ОШИБКА 404</h2>
-			<p>Страница не найдена</p>
+			<p className='text-lg'>Страница не найдена</p>
+			<button onClick={() => navigate('/')} className='w-1/3'>
+				Вернуться на главную
+			</button>
 		</div>
 	)
 }
